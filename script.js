e0dcdf3da6c2674ff9219d74ce78eafd80e6086e
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2022-03-25 22:44:35 +0000

    scrollable table

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-window.onload = main;
+window.onload = load_main;
 
 const epic_root = '/epic_root';
 const test_src = '/epic_root/P11/P11_105/frame_0000000001.jpg';
 const HEIGHT = 256; 
 const WIDTH = 456;
+
+const emap = new Map(); // map from ind to entry element
+let prev_hl_ind = null;  // hightlight index
 
 // convert int to 'frame_xxxx'
 function pad(num, size) {
@@ -60,14 +63,51 @@ function createElementWith(tag, xs, func=e=>e) {
     return func(elem);
 }
 
-function main() {
+function process_key(e) {
+    const key = e.key;
+    if (key == 40 || key == 'j') {   // hl DOWN
+        if (prev_hl_ind != null && prev_hl_ind < emap.size-1) {
+            emap.get(prev_hl_ind+1).scrollTo(1000, 100);
+            change_highlight(prev_hl_ind+1);
+            // const tab = document.getElementById('annotTable');
+            // tab.scrollTop = tab.scrollTop + e.height;
+            // tab.addEventListener("scroll", e => {
+            //     console.log(tab.scrollTop);
+            // });
+        }
+    } else if (key == 38 || key == 'k') { // hl UP
+        if (prev_hl_ind != null && prev_hl_ind > 0) {
+            change_highlight(prev_hl_ind-1);
+        }
+    }
+}
+
+function change_highlight(hl_ind) {
+    if (prev_hl_ind == null) {
+        emap.get(hl_ind).style.backgroundColor = "cyan";
+        prev_hl_ind = hl_ind;
+        return;
+    }
+    if (hl_ind == prev_hl_ind) {
+        emap.get(hl_ind).style.backgroundColor = "gray";
+        prev_hl_ind = null;
+        return;
+    } else {
+        emap.get(prev_hl_ind).style.backgroundColor = "gray";
+        emap.get(hl_ind).style.backgroundColor = "cyan";
+        prev_hl_ind = hl_ind;
+        return;
+    }
+}
+
+function load_main() {
     const annot_body = document.getElementById('annot_body');
 
     // Create this json using `annot_df.to_json(..., orient='records')
     fetch('annot_df.json')
         .then(resp => resp.json())
         .then(df => {
-            df.map( e => {
+            df.map( (e, ind) => {
                 const entry = createElementWith('tr', [
                     createElementText('td', e.narration_id),
                     createElementText('td', e.participant_id),
@@ -83,9 +123,27 @@ function main() {
                     createElementText('td', e.noun_class),
                     createElementText('td', e.all_nouns),
                     createElementText('td', e.all_noun_classes),
-                ])
+                ], e => {
+                    e.ind = ind;
+                    // e.addEventListener("mouseenter", () => {
+                    //     if (!e.selected)
+                    //         e.style.backgroundColor = "cyan";
+                    // })
+                    // e.addEventListener("mouseleave", () => {
+                    //     if (!e.selected)
+                    //         e.style.backgroundColor = "gray";
+                    // })
+                    e.addEventListener("click", () => {
+                        change_highlight(ind);
+                    })
+                    return e;
+                });
                 annot_body.append(entry);
+                emap.set(ind, entry);
+                return entry;
             })
         })
         .catch(e => console.log(e));
+
+    document.addEventListener('keypress', process_key);
 }
